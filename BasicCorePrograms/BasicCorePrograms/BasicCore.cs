a0ada77760500931c4d9b5afb17fb5319bbca2fa
hem_b <hemanthb93@gmail.com>
2022-05-19 07:06:14 +0530

    [Hemanth]:displays prime factors of a number

--- BasicCorePrograms/BasicCorePrograms/BasicCore.cs
+++ BasicCorePrograms/BasicCorePrograms/BasicCore.cs
@@ -25,5 +25,28 @@ namespace BasicCorePrograms
             }
             Console.WriteLine("Harmonic value of {0} is {1} ",n,sum);
         }
+
+        public void PrimeFactors(int number)
+        {
+            Console.WriteLine("Prime Factors are ----- ");
+            while(number%2 == 0)
+            {
+                Console.Write("2 ");
+                number = number/2;
+            }
+            for(int i = 3; i <= Math.Sqrt(number);i+=2)
+            {
+                while(number%i == 0)
+                {
+                    Console.Write(i + " ");
+                    number = number/i;
+                }
+                
+            }
+            if (number > 2)
+            {
+                Console.Write(number + " ");
+            }
+        }
     }
 }
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -3,6 +3,13 @@ BasicCorePrograms.BasicCore basicCore = new BasicCorePrograms.BasicCore();
 Console.WriteLine("Enter a number below 31 to get table of 2 to power number");
 int n = Convert.ToInt32(Console.ReadLine());
 basicCore.DisplayPowerOfTwo(n);
+Console.WriteLine();
+//calculates Harmonic value of n
 Console.WriteLine("Enter a number to get Harmonic value");
 int number = Convert.ToInt32(Console.ReadLine());
 basicCore.HarmonicValue(number);
+Console.WriteLine();
+//calculates Prime factorial of a number
+Console.WriteLine("Enter a number to get its prime factors");
+int no = Convert.ToInt32(Console.ReadLine());
+basicCore.PrimeFactors(no);
